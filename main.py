ae15a78a6c7bb061349e199e4959e5883a04b33d
Gabriel Weng <gabeweng@gmail.com>
2023-07-20 12:55:30 -0400

    image in 3 column

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Connectiv")
+st.subheader("A Marketplace to find teammates for competitions")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Darren Weng")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("Gabe Weng")
+   st.text("Gabe is so cool")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("Julian Weng")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
